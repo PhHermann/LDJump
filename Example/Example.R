ee05ce72d950c4af6bfe1055ed526bbdf312fa5e
Philipp Hermann <philipp.hermann@aon.at>
2017-11-02 15:12:52 +0100

    Update Example.R

--- Example/Example.R
+++ Example/Example.R
@@ -1,4 +1,6 @@
 require(LDJump)
 results = LDJump("/pathToSample/HatLandscapeN16Len1000000Nrhs15_th0.01_540_1.fa", alpha = 0.05, segLength = 1000, 
                  pathLDhat = "/pathToLDhat", format = "fasta", refName = NULL, thth = 0.01)
-plot(results)
+pdf("Results.pdf")
+plot(results[[1]])
+dev.off()
