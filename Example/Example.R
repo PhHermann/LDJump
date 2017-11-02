7127c9677afb9135c228d4823a97ab749b28a765
Philipp Hermann <philipp.hermann@aon.at>
2017-11-02 13:42:01 +0100

    Update Example.R

--- Example/Example.R
+++ Example/Example.R
@@ -1,4 +1,4 @@
 require(LDJump)
 results = LDJump("/pathToSample/HatLandscapeN16Len1000000Nrhs15_th0.01_540_1.fa", alpha = 0.05, segLength = 1000, 
-                 pathLDHat = "/pathToLDHat", format = "fasta", refName = NULL, thth = 0.005)
+                 pathLDhat = "/pathToLDhat", format = "fasta", refName = NULL, thth = 0.01)
 plot(results)
