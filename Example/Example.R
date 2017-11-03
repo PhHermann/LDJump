1bf1b880b668fc58cd3f76a2b37067b656a5f5e4
Philipp Hermann <philipp.hermann@aon.at>
2017-11-03 09:20:18 +0100

    Update Example.R

--- Example/Example.R
+++ Example/Example.R
@@ -2,5 +2,5 @@ require(LDJump)
 results = LDJump("/pathToSample/HatLandscapeN16Len1000000Nrhs15_th0.01_540_1.fa", alpha = 0.05, segLength = 1000, 
                  pathLDhat = "/pathToLDhat", format = "fasta", refName = NULL, thth = 0.01)
 postscript("Results.eps", horiz = F)
-plot(results[[1]])
+plot(results[[1]], xlab = "Segments", ylab = "Estimated Recombination Rate", main = "Estimated recombination map with LDJump")
 dev.off()
