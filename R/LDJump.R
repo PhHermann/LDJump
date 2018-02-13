ed43c0eaab5b7348837ea76c45cfb033293a577f
PhHermann <philipp.hermann@aon.at>
2018-02-13 16:03:35 +0100

    typo changes

--- R/LDJump.R
+++ R/LDJump.R
@@ -1,6 +1,6 @@
 LDJump = function(seqName = "", alpha = 0.05, segLength = 1000, pathLDhat = "", format = "fasta", refName = NULL, start = NULL, thth = 0.005, constant = F, rescale = F, status = T, polyThres = 0, cores = 1, accept = F) {
-  if(pathLDhat == "") {pathLDhat = system("locate LDhat-master | head -n 1", intern = T)}
-  if(length(pathLDhat == 0)) {stop("Beware that this package requires LDhat to be installed for usage.")}
+  if(pathLDhat == "") {pathLDhat = paste0(system("locate LDhat-master | head -n 1", intern = T), "/")}
+  if(length(pathLDhat) == 0) {stop("Beware that this package requires LDhat to be installed for usage.")}
   if(seqName == "") {stop("Please provide the path of the sequence files in fasta/vcf format.")}
   if(format == "vcf") {
     vcfR_to_fasta(seqName, refName, ext.ind = T, cons = F, ext.haps = T, start = start)
@@ -10,7 +10,6 @@ LDJump = function(seqName = "", alpha = 0.05, segLength = 1000, pathLDhat = "", 
     if(constant) {
       segLength = nn
     }
-
     ll = floor(width(s)[1]/segLength)*segLength
     segs = ll/segLength
     if(!check_continue(seqName, segs = segs, accept)) {return()}
