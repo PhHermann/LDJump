cb123feb459ecbf4c4091ce9926791125c319f0c
fardokhtsadat <fardokht.fm@gmail.com>
2019-08-22 11:56:34 +0200

    Changes in VCF_2_FASTA()
    This file underwent a complete change which will be needed to convert it from FASTA to VCF.

--- R/vcfR_to_fasta.R
+++ R/vcfR_to_fasta.R
@@ -1,10 +1,13 @@
-vcfR_to_fasta = function(seqName, refName = NULL, ext.ind = T, cons = F, ext.haps = T, start = NULL) {
+vcfR_to_fasta = function(seqName, refName = NULL, ext.ind = T, cons = F, ext.haps = T, start = NULL, , ref = NULL, fa_start= NULL, fa_end = NULL, attr_name = NULL) {
+
   pop = vcfR::read.vcfR(file = seqName, limit = 1e+07, nrows = -1, skip = 0, cols = NULL, convertNA = F, verbose = T)
-  ref = ape::read.dna(file = refName, format = "fasta")
-  pop.dnabin = vcfR::vcfR2DNAbin(pop, extract.indels = ext.ind, consensus = cons,
-                           extract.haps = ext.haps, ref.seq = ref,
-                           start.pos = start, verbose = TRUE)
-  ### with consensus = T we have 1 sequence per individual ####
+  
+  seqinr::write.fasta(sequences = ref[[attr_name]][fa_start:fa_end], names = names(ref), nbchar = 80, file.out = paste0("temp/", fa_start, "_", fa_end, "_out.fa"))
+  
+  pop.dnabin = vcfR::vcfR2DNAbin(pop, extract.indels = T, consensus = F,
+                                 extract.haps = T, ref.seq = read.dna(paste0("temp/", fa_start, "_", fa_end, "_out.fa"), format = "fasta"),
+                                 start.pos = start, verbose = TRUE)
+
   ape::write.dna(x = pop.dnabin, file = paste0(seqName, ".fasta", sep = ""), format = "fasta", colsep = "")
-  print(paste(seqName, "converted to fasta file: ", seqName, ".fasta", sep = ""))
+  print(paste(seqName, " converted to fasta file: ", seqName, ".fasta", sep = ""))
 }
